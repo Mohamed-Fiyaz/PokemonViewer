e15fe20c4ac374d1e0fe731a3400b952a099b7c4
Mohamed Fiyaz <mohamedfiyaz@gmail.com>
2025-04-13 18:57:54 +0530

    Initial Commit

---
+++ Pokemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by Mohamed Fiyaz on 13/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
